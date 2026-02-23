bc7c18202ac7e3a92477f355e7dab1d74f938633
aaaapai <89151134+aaaapai@users.noreply.github.com>
2026-02-23 23:28:33 +0800

    Update lwjgl3 to 3.4.2

--- LWJGL/build.gradle.kts
+++ LWJGL/build.gradle.kts
@@ -23,10 +23,10 @@ tasks.jar {
     })
     exclude("net/java/openjdk/cacio/ctc/**")
     manifest {
-        attributes("Manifest-Version" to "3.4.1")
+        attributes("Manifest-Version" to "3.4.2")
         attributes("Automatic-Module-Name" to "org.lwjgl")
         attributes("Specification-Title" to "Lightweight Java Game Library - Core")
-        attributes("Specification-Version" to "3.4.1")
+        attributes("Specification-Version" to "3.4.2")
         attributes("Specification-Vendor" to "lwjgl.org")
         attributes("Implementation-Title" to "lwjgl")
         attributes("Implementation-Version" to "SNAPSHOT")
